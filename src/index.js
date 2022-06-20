500464d5dd782d0d32b2eededcf83b380672faf6
winston kiprop <winstonekip50@gmail.com>
2022-06-20 10:00:13 +0300

    commit tree 7183952cec71628c172e5ca637728b66d56aac46

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const init = () => {
   
 }
+const inputForm = document.querySelector('form')
 
 document.addEventListener('DOMContentLoaded', init);
 document.addEventListener('DOMContentLoaded', init); 
